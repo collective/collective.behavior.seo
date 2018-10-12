5b5afe606b1ee2d09ccdfca25f5a9843e9fee0d0
Fred van Dijk <fredvd@gmail.com>
2018-10-12 14:27:20 +0200

    "Add behavior: SeoFields"

---
+++ src/collective/behavior/seo/behaviors/seo_fields.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+from collective.behavior.seo import _
+from plone import schema
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.dexterity.interfaces import IDexterityContent
+from plone.supermodel import model
+from zope.component import adapter
+from zope.interface import implementer
+from zope.interface import provider
+
+
+@provider(IFormFieldProvider)
+class ISeoFields(model.Schema):
+    """
+    """
+
+    project = schema.TextLine(
+        title=_(u'Project'),
+        description=_(u'Give in a project name'),
+        required=False,
+    )
+
+
+@implementer(ISeoFields)
+@adapter(IDexterityContent)
+class SeoFields(object):
+    def __init__(self, context):
+        self.context = context
+
+    @property
+    def project(self):
+        if hasattr(self.context, 'project'):
+            return self.context.project
+        return None
+
+    @project.setter
+    def project(self, value):
+        self.context.project = value
---
+++ src/collective/behavior/seo/tests/test_behavior_seo_fields.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from collective.behavior.seo.behaviors.seo_fields import ISeoFields
+from collective.behavior.seo.testing import COLLECTIVE_BEHAVIOR_SEO_INTEGRATION_TESTING  # noqa
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.behavior.interfaces import IBehavior
+from zope.component import getUtility
+
+import unittest
+
+
+class SeoFieldsIntegrationTest(unittest.TestCase):
+
+    layer = COLLECTIVE_BEHAVIOR_SEO_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_behavior_seo_fields(self):
+        behavior = getUtility(IBehavior, 'collective.behavior.seo.seo_fields')
+        self.assertEqual(
+            behavior.marker,
+            ISeoFields,
+        )
+        behavior_name = 'collective.behavior.seo.behaviors.seo_fields.ISeoFields'
+        behavior = getUtility(IBehavior, behavior_name)
+        self.assertEqual(
+            behavior.marker,
+            ISeoFields,
+        )
