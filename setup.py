1d98464c402f257b83d33e535e96027bf080c9ac
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2018-10-30 06:17:55 +0100

    Back to development: 1.0b2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.behavior.seo',
-    version='1.0b1',
+    version='1.0b2.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
