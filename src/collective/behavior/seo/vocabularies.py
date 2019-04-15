228eefa5e594dbf10a2adea46386dfb711586a39
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-04-15 15:36:05 +0200

    use @implementer decorator

--- src/collective/behavior/seo/vocabularies.py
+++ src/collective/behavior/seo/vocabularies.py
@@ -1,20 +1,20 @@
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.schema.interfaces import IVocabularyFactory
-from zope.interface import implements
+from zope.interface import implementer
 from plone import api
 from collective.behavior.seo.interfaces import ICollectiveBehaviorSeoSettings
 
 
+@implementer(IVocabularyFactory)
 class RobotsVocabulary(object):
-    implements(IVocabularyFactory)
-
     def __call__(self, context):
 
         items = []
         tags = api.portal.get_registry_record(
-            'robot_tags',
+            "robot_tags",
             interface=ICollectiveBehaviorSeoSettings,
-            default=u'index, follow')
+            default=u"index, follow",
+        )
         for tag in tags:
             items.append(SimpleTerm(tag, tag, tag))
 
