cc9989489b4499a04cb498ece2d25de9887f2374
Mikel Larreategi <mlarreategi@codesyntax.com>
2019-04-15 15:53:51 +0200

    make python3 compatible

--- src/collective/behavior/seo/browser/metafields.py
+++ src/collective/behavior/seo/browser/metafields.py
@@ -10,11 +10,13 @@ class MetaFieldsViewlet(common.DublinCoreViewlet):
         super(MetaFieldsViewlet, self).update()
 
         if ISEOFieldsMarker.providedBy(self.context):
-
+            # in python3 this is a dict_items instance
+            self.metatags = list(self.metatags)
             if self.context.seo_description:
                 for index, (key, value) in enumerate(self.metatags):
                     if key == 'description':
                         self.metatags.pop(index)
                         break
+
                 self.metatags.append(
                     ('description', self.context.seo_description))
