aadd0c0e924857c93ac7bdbf0284f8e3a24a95bf
Maurits van Rees <maurits@vanrees.org>
2020-08-17 17:20:47 +0200

    Preparing release 1.0.0
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@ Changelog
 =========
 
 
-1.0b2 (unreleased)
+1.0.0 (2020-08-17)
 ------------------
 
 - Make python3 compatible. [erral]
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.behavior.seo',
-    version='1.0b2.dev0',
+    version='1.0.0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
