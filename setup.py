902021ebe79ba32d26bba1b142216ab4320b4e2c
Fred van Dijk <fredvd@gmail.com>
2020-09-09 22:26:42 +0200

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.behavior.seo',
-    version='1.0.1',
+    version='1.0.2.dev0',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
