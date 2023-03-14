5f7d631ccb4c826b3c7061129703fb2b3e8467da
Maurits van Rees <maurits@vanrees.org>
2023-03-14 14:42:06 +0100

    Fixed installer tests.

--- src/collective/behavior/seo/tests/test_setup.py
+++ src/collective/behavior/seo/tests/test_setup.py
@@ -7,6 +7,11 @@ from collective.behavior.seo.testing import COLLECTIVE_BEHAVIOR_SEO_INTEGRATION_
 
 import unittest
 
+try:
+    from plone.base.utils import get_installer
+except ImportError:
+    from Products.CMFPlone.utils import get_installer
+
 
 class TestSetup(unittest.TestCase):
     """Test that collective.behavior.seo is properly installed."""
@@ -16,11 +21,11 @@ class TestSetup(unittest.TestCase):
     def setUp(self):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.installer = get_installer(self.portal)
 
     def test_product_installed(self):
         """Test if collective.behavior.seo is installed."""
-        self.assertTrue(self.installer.isProductInstalled(
+        self.assertTrue(self.installer.is_product_installed(
             'collective.behavior.seo'))
 
     def test_browserlayer(self):
@@ -39,15 +44,15 @@ class TestUninstall(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.installer = get_installer(self.portal)
         roles_before = api.user.get_roles(TEST_USER_ID)
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
-        self.installer.uninstallProducts(['collective.behavior.seo'])
+        self.installer.uninstall_product('collective.behavior.seo')
         setRoles(self.portal, TEST_USER_ID, roles_before)
 
     def test_product_uninstalled(self):
         """Test if collective.behavior.seo is cleanly uninstalled."""
-        self.assertFalse(self.installer.isProductInstalled(
+        self.assertFalse(self.installer.is_product_installed(
             'collective.behavior.seo'))
 
     def test_browserlayer_removed(self):
