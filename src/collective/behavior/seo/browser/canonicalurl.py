4204b1315fe36b169dbcf7a58b545ae72d2066aa
Mikel Larreategi <mlarreategi@codesyntax.com>
2023-06-14 15:51:23 +0200

    override existing canonical viewlet

--- src/collective/behavior/seo/browser/canonicalurl.py
+++ src/collective/behavior/seo/browser/canonicalurl.py
@@ -1,19 +1,25 @@
-from ..interfaces import ISEOFieldsMarker
-from plone.app.layout.viewlets import common
-from zope.component import getMultiAdapter
-
 import logging
 
+from plone.app.layout.links.viewlets import CanonicalURL
+from plone.memoize import view
+from zope.component import getMultiAdapter
+
+from ..interfaces import ISEOFieldsMarker
 
 logger = logging.getLogger("collective.behavior.seo")
 
-# Code mostly reused from https://github.com/simplesconsultoria/sc.seo
 
-
-class CanonicalUrlViewlet(common.ViewletBase):
-    """Renders the  <link rel="canonical"> tag if the ICanonicalUrl is applied
-    to the context
-    """
+class CanonicalUrlViewlet(CanonicalURL):
+    @view.memoize
+    def render(self):
+        if self.behavior and self.behavior.seo_canonical:
+            canonical_url = self.behavior.seo_canonical
+        else:
+            context_state = getMultiAdapter(
+                (self.context, self.request), name="plone_context_state"
+            )
+            canonical_url = context_state.canonical_object_url()
+        return '    <link rel="canonical" href="%s" />' % canonical_url
 
     def update(self):
         super().update()
@@ -21,16 +27,3 @@ class CanonicalUrlViewlet(common.ViewletBase):
             self.behavior = ISEOFieldsMarker(self.context)
         except TypeError:
             self.behavior = None
-
-    def available(self):
-        return True if self.behavior else False
-
-    def content(self):
-        context_state = getMultiAdapter(
-            (self.context, self.request), name="plone_context_state"
-        )
-        content = self.behavior.seo_canonical
-        # If there is no restriction, we explicity allow indexing
-        if not content:
-            return self.context.absolute_url()
-        return content
